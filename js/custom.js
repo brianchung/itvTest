d87eb445ad3757bb266cacbce0edadf69bf38b09
brian.c <brianchung.cy@gmail.com>
2016-04-23 21:38:06 +0800

    update
    - update start panel ui
    - update css

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,23 @@ $(document).on('click','#start',function(e) {
 	$('#startpanel').toggle();
 });
 
-$('#age').slider({
+/*$('#slider-age').slider({
 	formatter: function(value) {
 		return value;
 	}
-});
+});*/
+
+$(function() {
+    $( "#slider-age" ).slider({
+      range: "max",
+      min: 0,
+      max: 100,
+      value: 2,
+      slide: function( event, ui ) {
+        //$( "#amount" ).val( ui.value );
+		
+		$('#slider-age').tooltip(ui.value)
+      }
+    });
+    //$( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+  });
